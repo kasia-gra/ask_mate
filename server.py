6e2314b8f8df890d475fb4519480bda67463c197
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-05-18 18:55:08 +0200

    Add basic @/question handling and Add basic question.html file

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import data_manager
 
 app = Flask(__name__)
 
@@ -8,5 +9,41 @@ def hello():
     return "Hello World!"
 
 
+@app.route("/question", methods=["POST", "GET"])
+def add_question():
+    if request.method == "POST":
+        question_title = request.form["title"]
+        question_message = request.form["description"]
+        image_path = request.form["image"]
+        # save new element into CSV file and redirect
+        return redirect("/")
+    return render_template("question.html")
+
+
+@app.route("/question/<question_id>/edit", methods=["POST", "GET"])
+def edit_question(question_id):
+    # seek CSV file for entry with this ID and show this results in question_details
+    question_details = {
+        id: 1,
+        "Submission Time": 0,
+        "View number": 0,
+        "Vote number": 0,
+        "Question title": "Question title",
+        "Question message": "Question Message",
+        "Image Path": "Image Path"
+    }
+    if request.method == "POST":
+        question_title = request.form["title"]
+        question_message = request.form["description"]
+        image_path = request.form["image"]
+        # save modified elements into CSV file and redirect
+        return redirect("/")
+    return render_template("question.html", question_details=question_details)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(
+        host='127.0.0.1',
+        port=8000,
+        debug=True,
+    )
