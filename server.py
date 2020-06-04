f7b81f623e01afc3d21b7ad290dbcacea99ea9f0
Ecoloqy <r2y.szczepaniak@gmail.com>
2020-06-04 09:22:10 +0200

    Refactor server.py

--- server.py
+++ server.py
@@ -17,20 +17,13 @@ app.register_blueprint(tag)
 
 @app.route("/")
 def homepage():
-    message_max_length = 800
-    title_max_length = 53
     five_questions = data_manager.get_five_records("question")
     if request.method == 'POST':
         sort_by = request.form.get("sort_by")
         five_questions = data_manager.get_sorted_questions(sort_by)
     else:
         sort_by = "submission_time-DESC"
-    for question in five_questions:
-        question["number_of_answers"] = len(data_manager.get_answers_for_question(question.get("id")))
-        if len(question["title"]) >= title_max_length:
-            question["title"] = question["title"][:title_max_length] + "..."
-        if len(question["message"]) >= message_max_length:
-            question["message"] = question["message"][:message_max_length] + "..."
+    five_questions = prepare_questions_to_display(five_questions)
     search_phrase = request.args.get('search_phrase')
     if search_phrase:
         return search_for_questions(search_phrase)
@@ -39,24 +32,29 @@ def homepage():
 
 @app.route("/list", methods=['GET', 'POST'])
 def questions_list():
-    message_max_length = 800
-    title_max_length = 53
     sort_by = request.args.get('sort_by')
     if sort_by:
         criteria_and_direction = sort_by.split("-")
         all_questions = data_manager.get_sorted_questions(criteria_and_direction)
     else:
         all_questions = data_manager.get_all_records("question")
-    for question in all_questions:
-        question["number_of_answers"] = len(data_manager.get_answers_for_question(question.get("id")))
-        if len(question["title"]) >= title_max_length:
-            question["title"] = question["title"][:title_max_length] + "..."
-        if len(question["message"]) >= message_max_length:
-            question["message"] = question["message"][:message_max_length] + "..."
+    all_questions = prepare_questions_to_display(all_questions)
     search_phrase = request.args.get('search_phrase')
     if search_phrase:
         return search_for_questions(search_phrase)
     return render_template("question_list.html", all_questions=all_questions, sort_by=sort_by, search_phrase=search_phrase, is_homepage=False)
+
+
+def prepare_questions_to_display(all_questions):
+    message_max_length = 800
+    title_max_length = 53
+    for record in all_questions:
+        record["number_of_answers"] = len(data_manager.get_answers_for_question(record.get("id")))
+        if len(record["title"]) >= title_max_length:
+            record["title"] = record["title"][:title_max_length] + "..."
+        if len(record["message"]) >= message_max_length:
+            record["message"] = record["message"][:message_max_length] + "..."
+    return all_questions
 
 
 @app.route("/question/<question_id>")
