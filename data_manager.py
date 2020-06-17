ccf4d74d06dc52d6980a9f5dc2f12c3c36dbcfed
StolarzPrzemyslaw <stolarz.przemyslaw@gmail.com>
2020-06-17 19:01:30 +0200

    commit for 27, 28, 29 features: add tables with questions, answers and comments to user page, also simple user panel

--- data_manager.py
+++ data_manager.py
@@ -456,3 +456,39 @@ def get_user_by_id(cursor: RealDictCursor, user_id: int):
     """
     cursor.execute(query, {"user_id": user_id})
     return cursor.fetchone()
+
+
+@connection.connection_handler
+def get_questions_by_user_id(cursor: RealDictCursor, user_id: int):
+    query = """
+    SELECT id, submission_time, view_number, vote_number, title, message, image, user_id
+    FROM question
+    WHERE user_id = %(user_id)s
+    ORDER BY id
+    """
+    cursor.execute(query, {'user_id': user_id})
+    return cursor.fetchall()
+
+
+@connection.connection_handler
+def get_answers_by_user_id(cursor: RealDictCursor, user_id: int):
+    query = """
+    SELECT id, submission_time, vote_number, question_id, message, image, user_id
+    FROM answer
+    WHERE user_id = %(user_id)s
+    ORDER BY id
+    """
+    cursor.execute(query, {'user_id': user_id})
+    return cursor.fetchall()
+
+
+@connection.connection_handler
+def get_comments_by_user_id(cursor: RealDictCursor, user_id: int):
+    query = """
+    SELECT id, question_id, answer_id, message, submission_time, edited_number, user_id
+    FROM comment
+    WHERE user_id = %(user_id)s
+    ORDER BY id
+    """
+    cursor.execute(query, {'user_id': user_id})
+    return cursor.fetchall()
